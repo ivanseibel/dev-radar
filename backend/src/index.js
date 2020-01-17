1c3ff95fd9d93e353818d661fd326371dde45ee5
Ivan Luis Seibel <ivanseibel@gmail.com>
2020-01-17 09:26:59 -0300

    Revert "Client-side websocket"
    This reverts commit 73d33ab43076620fba6e7e420df8c3b4d540a5ea.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes.js');
-const http = require('http');
-const { setupWebsocket } = require('./websocket');
 
 const mongoParams = require('../mongo-params.json');
 
@@ -15,13 +13,10 @@ mongoose.connect(mongoParams.stringConnection,
   });
 
 const app = express();
-const server = http.Server(app);
-
-setupWebsocket(server);
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
 
-server.listen('3333');
+app.listen('3333');
